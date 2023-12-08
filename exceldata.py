8f4bfcfc6f45f5e249e5775125bb4cdc56787b32
Muszchrom <altair2468@o2.pl>
2023-12-08 15:25:30 +0100

    Moved ExcelData class to different file

---
+++ exceldata.py
@@ -0,0 +1,47 @@
+import xlrd
+
+
+class ExcelData:
+    def __init__(self):
+        self.excel_files = [
+            "200k-30.xls",
+            "200k-40.xls",
+            "200k-50.xls",
+            "200k-60.xls",
+            "200k-70.xls",
+            "20k-30.xls",
+            "20k-40.xls",
+            "20k-50.xls",
+            "20k-60.xls",
+            "20k-70.xls",
+            "2k-30.xls",
+            "2k-40.xls",
+            "2k-50.xls",
+            "2k-60.xls",
+            "2k-70.xls"
+        ]
+        self.path = r"C:\Users\altai\Desktop\IZI 5\metrologia\stuff\\"
+
+    def get_file_names(self):
+        return self.excel_files
+
+    def get_file_name(self):
+        return self.excel_files[0]
+
+    def get_path(self):
+        return self.path
+
+    def get_values(self, filename):
+        xls = xlrd.open_workbook(self.path + filename).sheet_by_index(0)
+        time = []
+        ch1_vals = []
+        ch2_vals = []
+        for i in range(8, 5008):
+            time.append(float(xls.cell_value(rowx=i, colx=1)[:-2]))
+            ch1_vals.append(int(xls.cell_value(rowx=i, colx=2)))
+            ch2_vals.append(int(xls.cell_value(rowx=i, colx=3)))
+        return {
+            "time": time,
+            "ch1_vals": ch1_vals,
+            "ch2_vals": ch2_vals
+        }
--- front.py
+++ front.py
@@ -1,53 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+from exceldata import ExcelData
 
-import xlrd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib import pyplot as plt
-
-
-class ExcelData:
-    def __init__(self):
-        self.excel_files = [
-            "200k-30.xls",
-            "200k-40.xls",
-            "200k-50.xls",
-            "200k-60.xls",
-            "200k-70.xls",
-            "20k-30.xls",
-            "20k-40.xls",
-            "20k-50.xls",
-            "20k-60.xls",
-            "20k-70.xls",
-            "2k-30.xls",
-            "2k-40.xls",
-            "2k-50.xls",
-            "2k-60.xls",
-            "2k-70.xls"
-        ]
-        self.path = r"C:\Users\altai\Desktop\IZI 5\metrologia\stuff\\"
-    def getFileNames(self):
-        return self.excel_files
-    def getFileName(self):
-        return self.excel_files[0]
-    def getPath(self):
-        return self.path
-    def getValues(self, filename):
-        xls = xlrd.open_workbook(self.path + filename).sheet_by_index(0)
-        time = []
-        ch1_vals = []
-        ch2_vals = []
-        for i in range(8, 5008):
-            time.append(float(xls.cell_value(rowx=i, colx=1)[:-2]))
-            ch1_vals.append(int(xls.cell_value(rowx=i, colx=2)))
-            ch2_vals.append(int(xls.cell_value(rowx=i, colx=3)))
-        return {
-            "time": time,
-            "ch1_vals": ch1_vals,
-            "ch2_vals": ch2_vals
-        }
-
-
 
 class Front(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -59,9 +15,9 @@ class Front(tk.Tk):
         self.right_container = tk.Frame(self)
         self.right_container.pack(side="right")
 
-        self.combo = ttk.Combobox(self.right_container, values=self.excInst.getFileNames(), state='readonly')
+        self.combo = ttk.Combobox(self.right_container, values=self.excInst.get_file_names(), state='readonly')
         self.combo.bind('<<ComboboxSelected>>', self.on_combo_change)
-        self.combo.set(self.excInst.getFileName())
+        self.combo.set(self.excInst.get_file_name())
         self.combo.pack(side="top", padx=10, pady=10)
 
         self.checkbox_checked = tk.IntVar()
@@ -76,7 +32,7 @@ class Front(tk.Tk):
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.container)
         filename = self.combo.get()
-        self.data = self.excInst.getValues(filename)
+        self.data = self.excInst.get_values(filename)
         self.ax.plot(self.data["time"], self.data["ch1_vals"], self.data["time"], self.data["ch2_vals"])
 
         self.h_line_1 = self.ax.axhline(color='g', lw=2, ls='--')
@@ -87,7 +43,6 @@ class Front(tk.Tk):
         self.canvas.get_tk_widget().pack(side="left")
         self.canvas.mpl_connect('motion_notify_event', self.on_mouse_move)
         self.canvas.mpl_connect('key_press_event', self.on_key_press)
-
 
         self.key_first_pressed = False
         self.key_second_pressed = False
@@ -136,6 +91,7 @@ class Front(tk.Tk):
         diffv = abs(v_1 - v_2)
         self.h_delta_info.config(text=f"ΔH={diffh}")
         self.v_delta_info.config(text=f"ΔV={diffv}")
+
     def on_mouse_move(self, event):
         x, y = event.xdata, event.ydata
         if self.key_first_pressed:
@@ -151,12 +107,13 @@ class Front(tk.Tk):
     def on_combo_change(self, event):
         filename = self.combo.get()
         checkbox_state = self.checkbox_checked.get()
-        self.data = self.excInst.getValues(filename)
+        self.data = self.excInst.get_values(filename)
         if checkbox_state:
             self.plot_lissajous()
         else:
             self.plot_graph()
         self.draw_grid()
+
     def plot_lissajous(self):
         self.ax.clear()
         self.ax.scatter(self.data["ch1_vals"], self.data["ch2_vals"], s=.25)
@@ -169,4 +126,6 @@ class Front(tk.Tk):
         self.ax.plot(self.data["time"], self.data["ch2_vals"])
         self.draw_grid()
         self.draw_h_v_lines()
+
+
 Front().mainloop()
